0507ed4f9eb2b2a5657f7cebc4ad554e305565ad
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-07-12 15:42:45 +1200

    Next app init.

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-// Get __dirname equivalent in ES module context
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-    experimental: {
-        appDir: join(__dirname, 'src/app'),
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
-
-
