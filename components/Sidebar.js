67f0c8d8bcf3e2a95f573b9e514782324a41b2a3
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-07-12 16:37:26 +1200

    Added layout for sidebar.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,46 @@
+import {
+    HomeIcon,
+    SearchIcon,
+    PlusCircleIcon,
+    LibraryIcon,
+    RssIcon,
+    HeartIcon,
+} from '@heroicons/react/outline'
+
 function Sidebar() {
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <div className='text-gray-500 p-5 text0sm border-r border-gray-900'>
+      
+      <div className='space-y-4'>
+        <button className='flex  items-center space-x-2 hover:text-white'>
+            <HomeIcon className='h-5 w-5'/>
+            <p>Home</p>
+        </button>
+
+        <button className='flex  items-center space-x-2 hover:text-white'>
+            <SearchIcon className='h-5 w-5'/>
+            <p>Search</p>
+        </button>
+        <button className='flex  items-center space-x-2 hover:text-white'>
+            <LibraryIcon className='h-5 w-5'/>
+            <p>Your Library</p>
+        </button>
+      <hr className='border-t-[0.1px] border-gray-900 '/>
+        <button className='flex  items-center space-x-2 hover:text-white'>
+            <PlusCircleIcon className='h-5 w-5'/>
+            <p>Create Playlist</p>
+        </button>
+
+        <button className='flex  items-center space-x-2 hover:text-white'>
+            <RssIcon className='h-5 w-5'/>
+            <p>Your episodes</p>
+        </button>
+        <button className='flex  items-center space-x-2 hover:text-white'>
+            <HeartIcon className='h-5 w-5'/>
+            <p>Liked Songs</p>
+        </button>
+      <hr className='border-t-[0.1px] border-gray-900 '/>
+      </div>
     </div>
   )
 }
