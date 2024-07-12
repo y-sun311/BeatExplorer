785f4df37e95e8a562b49b2a3796c70ce3dcae0c
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-07-12 17:11:20 +1200

    Added /lib for scopes and login_url;installed spotify-web-api package.

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -1,13 +1,18 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import { LOGIN_URL } from "../../lib/spotify";
 
 
 export default NextAuth({
     providers: [
         SpotifyProvider({
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        authorization
+        clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+        clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+        authorization : LOGIN_URL,
         }),
     ],
+    secret: process.env.JWT_SECRET,
+    pages: {
+        signIn: "/login",
+    },
     });
